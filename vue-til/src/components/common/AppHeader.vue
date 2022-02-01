519c6dddce86d0650c4a26433736ba72d03edef3
DINGU <web.ljg@gmail.com>
2022-02-01 12:56:38 +0900

    jsconfig.json을 활용한 파일 import

---
+++ vue-til/src/components/common/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>header</div>
+</template>
+
+<script>
+import Demo from '@/demo/basic/Demo.vue';
+
+export default {
+  components: {
+    Demo,
+  },
+};
+</script>
+
+<style></style>
