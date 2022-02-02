fb5ba22b50d65a325e8c9beca60779c5a542af61
DINGU <web.ljg@gmail.com>
2022-02-02 17:53:29 +0900

    # 프로젝트 구성, 뷰 라우터 설치

--- vue-til/src/components/common/AppHeader.vue
+++ vue-til/src/components/common/AppHeader.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>header</div>
+	<div>header</div>
 </template>
 
 <script>
-import Demo from '@/demo/basic/Demo.vue';
+import Demo from '../../demo/basic/Demo'
+import Demo from '@/demo/basic/Demo'
+// import Demo from '@/demo/basic/Demo'
 
-export default {
-  components: {
-    Demo,
-  },
-};
+export default {};
 </script>
 
 <style></style>
