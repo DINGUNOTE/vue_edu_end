3e7359fd2d4ef75f56e23e47578c937ff4b4f260
DINGU <web.ljg@gmail.com>
2022-02-11 14:37:58 +0900

    # API 함수 모듈화 # 학습 노트 삭제 기능 추가 # 학습 노트 수정 기능 추가

--- vue-til/src/components/posts/PostEditForm.vue
+++ vue-til/src/components/posts/PostEditForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="contents">
-    <h1 class="page-header">Create Post</h1>
+    <h1 class="page-header">Edit Post</h1>
     <div class="form-wrapper">
       <form class="form" @submit.prevent="submitForm">
         <div>
@@ -14,7 +14,7 @@
             Contents length must be less than 200
           </p>
         </div>
-        <button type="submit" class="btn">Create</button>
+        <button type="submit" class="btn">Edit</button>
       </form>
       <p class="log">
         {{ logMessage }}
@@ -24,8 +24,7 @@
 </template>
 
 <script>
-import { createPost } from '@/api/posts';
-
+import { fetchPost, editPost } from '@/api/posts';
 export default {
   data() {
     return {
@@ -41,17 +40,25 @@ export default {
   },
   methods: {
     async submitForm() {
+      const id = this.$route.params.id;
       try {
-        const response = await createPost({
+        await editPost(id, {
           title: this.title,
           contents: this.contents,
         });
         this.$router.push('/main');
-        console.log(response);
       } catch (error) {
-        this.logMessage = error.response.data.message;
+        console.log(error);
+        this.logMessage = error;
       }
     },
+  },
+  async created() {
+    const id = this.$route.params.id;
+    const { data } = await fetchPost(id);
+    this.title = data.title;
+    this.contents = data.contents;
+    // console.log(data);
   },
 };
 </script>
